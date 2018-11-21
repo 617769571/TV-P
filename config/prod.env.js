83c922666477f638b285987fd71fbf22ac37dd30
617769571@qq.com <617769571@qq.com>
2018-11-21 14:54:08 +0800

    add

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,8 +2,7 @@
 
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.14.86:8080/ott-manage/rest"',
-  GAIA_API: '"http://192.168.14.15:8080/gaia/rest"',
-  VALID_IMG_URL: '"http://192.168.14.15:8080/gaia/rest/user-login/verifyImage/"',
-  IMG_URL:'"http://192.168.14.86:80/ott"'
+  BASE_API: '"http://central.weherepost.com:1215/ott-manage/rest"',
+  GAIA_API: '"http://central.weherepost.com:1210/gaia/rest"',
+  VALID_IMG_URL: '"http://central.weherepost.com:1210/gaia/rest/user-login/verifyImage/"'
 }
